e741df0dfff0ea264c5d7cb0fcd685607e4287a5
Roger Diller <roger@flexrentalsolutions.com>
2017-12-02 20:31:29 -0500

    "First" component in the books!

---
+++ src/components/First.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="first">
+    <p>First!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'First'
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
